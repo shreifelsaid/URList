66bd5ea2761e8c1f7caae206a446c7cc2e22306c
Shreif Abdallah <selsaid@u.rochester.edu>
2020-12-06 14:11:06 -0500

    UPDATE : password reset added to profile

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
 from flask.helpers import flash
+from werkzeug import security
 from flask_login import  login_user, logout_user, login_required, current_user
 from models import user, db
 from forms import LoginForm, ResetPasswordForm, SignupForm
@@ -41,27 +42,32 @@ def login_post():
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
-    email = username = password   = None
+    email = username = password = security_question = security_answer  = None
     signup_form = SignupForm()
     if signup_form.validate_on_submit():
         print("validated succcessfully")
         email = signup_form.email.data
         username = signup_form.username.data
         password = signup_form.password.data
+        security_question = signup_form.security_question.data
+        security_answer = signup_form.security_answer.data
         existing_user = user.query.filter((email == user.email) | (username == user.username)).first()
         if existing_user :
             print("---------------")
             print(existing_user.email)
             print(existing_user.username)
-            print(existing_user)
+            print(existing_user.security_question)
+            print(existing_user.security_answer)
             print("---------------")
             print(signup_form.email.data)
             print(signup_form.username.data)
             print(signup_form.password.data)
+            print(signup_form.security_question.data)
+            print(signup_form.security_answer.data)
 
             flash("Username or email is already in use, log in!")
             return redirect(url_for('auth.login'))
-        db_entry = user(email=email, username=username, password=generate_password_hash(password, method='sha256'))
+        db_entry = user(email=email, username=username, password=generate_password_hash(password, method='sha256'),security_question=security_question, security_answer=generate_password_hash(security_answer, method='sha256'))
         try:
             db.session.add(db_entry)
             db.session.commit()
@@ -73,4 +79,31 @@ def signup_post():
 @login_required
 def profile():
     password_reset_form = ResetPasswordForm()
-    return  render_template('profile.html',current_user=current_user, password_reset_form=password_reset_form)
+    existing_user = user.query.filter(email = current_user.email).first()
+    security_question_dict = {
+    1 : 'What was your childhood nickname?',
+    2 : 'What is the name of your favorite childhood friend?',
+    3 : 'What was the last name of your third grade teacher?',
+    4 : 'In what city or town was your first job?',
+    5 : 'What was the name of your first stuffed animal?',
+    6 : 'What is the name of a college you applied to but didn\'t attend?',
+    }
+    security_question = security_question_dict[existing_user.security_question]
+    existing_answer = existing_user.security_answer
+    existing_password = existing_user.password
+    if password_reset_form.validate_on_submit():
+        security_answer = password_reset_form.security_answer.data
+        old_password = password_reset_form.old_password.data
+        new_password = password_reset_form.new_password
+
+        if not check_password_hash(existing_password, old_password) or not check_password_hash(existing_answer, security_answer):
+            flash("wrong password or security answer")
+            return redirect(url_for('auth.profile'))
+            
+        existing_user.password = generate_password_hash(new_password, method='sha256')
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Update error"
+    return  render_template('profile.html',current_user=current_user, password_reset_form=password_reset_form,security_question=security_question)
