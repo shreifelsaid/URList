578051b983b719cad21353c2b7e94bfa0081dc77
Emanuela Natali <enatali@u.rochester.edu>
2020-12-07 16:30:14 -0500

    Cart feature front end

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def index():
             return "Database error"
     else :
         print(post_form.errors)
-
-    return render_template('index.html', post_form=post_form , posts_table=posts.query.order_by(posts.id.desc()).all(), current_user=current_user,cart_list = [item.post_id for item in cart_list()])
+    items_list = [item.post_id for item in cart_list()]
+    return render_template('index.html', post_form=post_form , posts_table=posts.query.order_by(posts.id.desc()).all(), current_user=current_user,cart_list =items_list, len_cart_list = len(items_list) )
 
 
